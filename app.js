e90b7e17109d4df3d1fce3dc7c1588ec25e82934
Marcus Dalgren <marcus@raket.nu>
2014-02-04 23:40:51 +0100

    Catch root

--- app.js
+++ app.js
@@ -15,5 +15,11 @@ app.get('/hello', function(req, res){
 	res.setHeader('Content-Length', Buffer.byteLength(body));
 	res.end(body);
 });
+app.get("/", function(req, res) {
+	var body = 'Hello World';
+	res.setHeader('Content-Type', 'text/plain');
+	res.setHeader('Content-Length', Buffer.byteLength(body));
+	res.end(body);
+});
 app.listen('/tmp/node.sock');
 console.log('Listening on nginx socket');
